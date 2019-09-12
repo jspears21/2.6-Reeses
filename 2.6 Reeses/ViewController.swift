6e4cca05eb49d43ce8ed67eaeda9cccf1c53ddd7
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-11 18:15:10 -0700

    hi

--- 2.6 Reeses/ViewController.swift
+++ 2.6 Reeses/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.sizeToFit()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessage(_ sender: UIButton) {
+        let msg1 = "You Got Peanut Butter in My Chocolate"
+        let msg2 = "You Got Chocolate in My Peanut Butter"
+        if messageLabel.text == msg1 {
+            messageLabel.text = msg2
+            messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = NSTextAlignment.right
+        } else {
+            messageLabel.text = msg1
+            messageLabel.textColor = UIColor.brown
+            messageLabel.textAlignment = NSTextAlignment.left
+        }
+        
+    }
+    
 }
 
